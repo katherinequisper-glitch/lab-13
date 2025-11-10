84eb75704a5412de22a3bdd746c1a70ce8945f9c
katherinequisper-glitch <katherine.quispe.r@tecsup.edu.pe>
2025-11-10 12:18:56 -0500

    final commit

--- database/migrations/2025_11_10_144215_create_recordatorios_table.php
+++ database/migrations/2025_11_10_144215_create_recordatorios_table.php
@@ -6,38 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-<<<<<<< HEAD
-=======
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
->>>>>>> 5d98d8a6a0cadc7fdc842dd338c9908a820fcd03
     public function up(): void
     {
         Schema::create('recordatorios', function (Blueprint $table) {
             $table->id();
             $table->foreignId('nota_id')->constrained()->onDelete('cascade');
-<<<<<<< HEAD
             $table->dateTime('fecha_vencimiento');
             $table->boolean('completado')->default(false);
-=======
-            $table->dateTime('fecha_vencimiento'); // Cambiado 'due_date' a 'fecha_vencimiento'
-            $table->boolean('completado')->default(false); // Cambiado 'is_completed' a 'completado'
->>>>>>> 5d98d8a6a0cadc7fdc842dd338c9908a820fcd03
             $table->timestamps();
         });
     }
 
-<<<<<<< HEAD
-=======
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
->>>>>>> 5d98d8a6a0cadc7fdc842dd338c9908a820fcd03
     public function down(): void
     {
         Schema::dropIfExists('recordatorios');
